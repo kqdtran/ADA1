5a697dc8017222d1fa1eb0ac3d02df590120258f
Khoa Tran <khoatran@berkeley.edu>
2013-01-04 12:37:43 +0700

    Added some optimizations to dijkstra's shortest path implementation

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -68,25 +68,24 @@ def dijkstra(G, start, end=None):
     Input: G - the input graph in the adjacency list representation via a dictionary
     start - the starting vertex
     end - the ending vertex. It is not necessary to provide this argument, in that case dijkstra's will find the distance
-    from start to every other vertex, but that may take a long time, so it is recommended to provide the last argument
-    """
+    from 'start' to every other vertex, but that may take a long time, so it is recommended to provide the last argument"""
 
-    D = {}	# dictionary of final distances
-    P = {}	# dictionary of predecessors
-
+    D = {}	          # dictionary of final distances
+    P = {}	          # dictionary of predecessors
+    Q = priority_dict()   # est.dist. of non-final vert.
+    
     # initialize D and P
     for vertex in G:
-        D[vertex] = float("inf")
+        Q[vertex] = float("inf")
         P[vertex] = None
     
-    Q = priority_dict()   # est.dist. of non-final vert.
     Q[start] = 0
     
-    for v in Q:
+    for v in Q: # iterate and pop the smallest item in Q
         D[v] = Q[v]
-        if v == end: break
-        
-        for w in G[v]:
+        if v == end: break # we have reached the end
+
+        for w in G[v]: # for all of v's adjacent vertices
             vwLength = D[v] + G[v][w]
             if w not in Q or vwLength < Q[w]:
                 Q[w] = vwLength
@@ -100,7 +99,11 @@ def findshortestPath(G, start, end):
     The output is a list of the vertices in order along
     the shortest path.
 
-    This method is not needed in this current exercise, however, it would be nice to know the shortest path from one vertex to another sometimes"""
+    Input: G - the input graph in the adjacency list representation via a dictionary
+    start - the starting vertex
+    end - the ending vertex
+
+    Note: This method is not needed in the current exercise, however, it would be nice to know the shortest path from one vertex to another sometimes"""
 
     _, P = dijkstra(G, start, end)
     path = []
@@ -108,7 +111,7 @@ def findshortestPath(G, start, end):
         path.append(end)
         if end == start: break
         end = P[end] # find the next predecessor
-    path.reverse() 
+    path.reverse() # reverse the list since we are appending from the back
     return path
     
 def main():
@@ -116,8 +119,10 @@ def main():
     lst = [7, 37, 59, 82, 99, 115, 133, 165, 188, 197] # a list of all the desired ending vertices
     for v in lst:
         D, _ = dijkstra(G, 1, v)
-        print(D[v], end=",")
-    print()
+        if v != lst[-1]:
+            print(D[v], end=",")
+        else:
+            print(D[v])
     
 if __name__ == '__main__':
     main()
